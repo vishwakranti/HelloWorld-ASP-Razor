15299374046f60550c07ee34cb89810c74872ce3
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-07-22 21:31:04 +1200

    Add Comments

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,8 +5,8 @@ namespace HelloWorld_ASP_Razor.Pages
 {
     public class IndexModel : PageModel
     {
-       
-
+       //This is the comment
+        IServerSideBlazorBuilder
         public void OnGet()
         {
 
